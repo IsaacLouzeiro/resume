db437509803d3b035dbfdc1fb72e46aef843439c
Isaac E. Louzeiro <isaacelias1110@gmail.com>
2022-01-20 21:27:50 -0300

    iniciando conteudo do header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,57 @@
 <template>
-    <h1>Hi</h1>
+    <section id="paper">
+        <HeaderComp />
+    </section>
 </template>
 
 <script>
+import HeaderComp from './components/HeaderComp.vue';
+
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        HeaderComp
+    }
 }
 </script>
 
 <style lang="scss">
+    @import './assets/sass/colors.scss';
 
+    * { 
+        margin: 0; 
+        padding: 0;
+        -moz-box-sizing: border-box; 
+        -webkit-box-sizing: border-box; 
+        box-sizing: border-box;
+        font-family: 'Ubuntu', sans-serif;
+    }
+
+    body, html, #app {
+        min-height: 100%;
+    }
+    
+    #app {
+        background-color: $color3;
+        color: $color1;
+        padding: 2em 0;
+        display: flex;
+        justify-content: center;
+
+        @media only screen and (max-width: 794px) {
+            padding: 0;
+        }
+    }
+
+    #paper {
+        background-color: $color5;
+        width: 100%;
+        max-width: 794px;
+        height: 1123px;
+        padding: 1.7em;
+
+        @media only screen and (max-width: 650px) {
+            padding: 10px;
+        }
+    }
 </style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,62 @@
+<template>
+    <header>
+        <div class="details">
+            <h1>{{ nome }}</h1>
+            <span>{{ nomeCompleto }}, {{ idade }} | {{ local }}</span>
+            <h4>{{ profissao }}</h4>
+        </div>
+        <div class="image"></div>
+    </header>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComp',
+    data() {
+        return {
+            nome: 'Isaac Louzeiro',
+            nomeCompleto: 'Isaac Elias Louzeiro Silva das Virgens',
+            idade: '19 anos',
+            local: 'São Paulo, SP',
+            profissao: 'Desenvolvedor Front-End Jr'
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import '../assets/sass/colors.scss';
+
+    header {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding-bottom: 1.5em;
+        border-bottom: 1px solid $color4;
+
+        .details {
+            span {
+                font-style: italic;
+                color: $color3;
+            }
+            h4 {
+                margin-top: 5px;
+            }
+        }
+
+        .image {
+            background-image: url('../assets/img/my-image.webp');
+            background-size: cover;
+            width: 130px;
+            height: 130px;
+            margin-left: 10px;
+            border-radius: 100%;
+
+            @media only screen and (max-width: 650px) {
+                width: 100px;
+                min-width: 100px;
+                height: 100px;
+            }
+        }
+    }
+</style>
